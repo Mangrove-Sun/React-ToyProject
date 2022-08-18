b90850152e16af0020cc4edfe7f2835250a36304
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-19 00:07:07 +0900

    feat: get data to use api & print

--- src/components/DustInfoCard.jsx
+++ src/components/DustInfoCard.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import * as S from './style';
 
 function DustInfoCard() {
+
+  const getParameters = {
+    serviceKey: process.env.REACT_APP_SERVICE_KEY,
+    returnType: 'json',
+    numOfRows: '100',
+    pageNo: '1',
+    sidoName: '서울',
+    ver: '1.0'
+  }
+
+  const [data, setData] = useState([]);
+  const getData = async() => {
+    try {
+      const res = await axios.get('B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty', {params: getParameters});
+      setData(res.data.response.body.items);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div>DustInfoCard</div>
+    <div>
+      {data && data.map(element => {
+        return (
+          <S.Card color={element.pm10Grade}  key={element.stationName}>
+            <div className="location">
+              <S.Title>{element.stationName}</S.Title><S.Sido className="sido">{element.sidoName}</S.Sido>
+              
+            </div>
+            <S.Info className="info">
+              <S.DustLv color={element.pm10Grade} className="dust-lv">
+                {element.pm10Grade === '1' ? '좋음' 
+                  : element.pm10Grade === '2' ? '보통' 
+                  : element.pm10Grade === '3' ? '한때나쁨' 
+                  : element.pm10Grade === '4' ? '나쁨' 
+                  : element.pm10Grade === '5' ? '매우나쁨' : '알수없음'}
+              </S.DustLv>
+              <div className="dust-stats">
+                <S.DustInfo>미세먼지 수치: {element.pm10Value}</S.DustInfo>
+                <S.DustInfo>({element.dataTime} 기준)</S.DustInfo>
+              </div>
+            </S.Info>
+          </S.Card>
+        )
+      })}
+    </div>
   )
 }
 
---
+++ src/components/style.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  padding: 12px;
+  margin: 0 auto;
+  margin-top: 28px;
+  max-width: 250px;
+  color: white;
+  background-color: 
+    ${props => props.color === '1' ? 'skyblue' 
+    : props.color === '2' ? 'green' 
+    : props.color === '3' ? 'yellow' 
+    : props.color === '4' ? 'orange' 
+    : props.color === '5' ? 'red' : 'darkgray'};
+  border-radius: 7px;
+  box-shadow: 1px 1px 6px #aaa;
+`
+
+export const Title = styled.h4`
+  display: inline-block;
+  padding: 0;
+  margin: 0;
+  margin-right: 5px;
+  font-weight: normal;
+`
+
+export const Sido = styled.span`
+  font-size: 12px;
+`
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const DustLv = styled.div`
+  margin-top: 10px;
+  padding: 5px;
+  font-size: 35px;
+  line-height: 40px;
+  color: 
+    ${props => props.color === '1' ? 'skyblue' 
+    : props.color === '2' ? 'green' 
+    : props.color === '3' ? 'yellow' 
+    : props.color === '4' ? 'orange' 
+    : props.color === '5' ? 'red' : 'darkgray'};
+  background-color: #fff;
+  border-radius: 7px;
+`
+
+export const DustInfo = styled.p`
+  margin-top: 8px;
+  text-align: center;
+  font-size: 12px;
+`
